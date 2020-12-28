448580460e098ec76d266d10973ebf1df2e65a88
sohrin <s.murase@sanyu-i.com>
2020-12-28 21:01:37 +0900

    敵に攻撃したとき、倒したときのスプライト切り替えを実装。

--- Assets/Scripts/BattleDataScript.cs
+++ Assets/Scripts/BattleDataScript.cs
@@ -8,17 +8,15 @@ public class BattleDataScript : MonoBehaviour
 {
     public EnemyData enemyData;
     private int enemyHp;
-
     private int enemyIdx = 0;
-
     private List<EnemyData> enemyList;
-    private static readonly int ENEMY_LIST_MAX_IDX = 1;
+    private static readonly int ENEMY_LIST_MAX_SIZE = 1;
     public Text hpText;
     public Text enemyNameText;
     public SpriteRenderer mainSpriteRenderer;
-    public Sprite dwarfSprite;
-    public Sprite behemothSprite;
-    public Sprite elementalSprite;
+
+    int damagedRemainingFrame = 0;
+    int defeatedRemainingFrame = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -34,20 +32,9 @@ public class BattleDataScript : MonoBehaviour
         enemy.hp = 10;
         enemy.atk = 1;
         enemy.def = 1;
-        enemy.normalSprite = Resources.Load<Sprite>("Enemies/sample/Static/Front/Megapack III Axe Warrior Dwarf");
-        enemy.damagedSprite = Resources.Load<Sprite>("Enemies/sample/Static/Front/Megapack III Axe Warrior Dwarf");
-        enemy.defeatedSprite = Resources.Load<Sprite>("Enemies/sample/Static/Front/Megapack III Axe Warrior Dwarf");
-        enemyList.Add(enemy);
-
-        // test
-        enemy = new EnemyData();
-        enemy.name = "test";
-        enemy.hp = 15;
-        enemy.atk = 2;
-        enemy.def = 3;
-        enemy.normalSprite = Resources.Load<Sprite>("Enemies/sample/Static/Front/Megapack III Behemoth");
-        enemy.damagedSprite = Resources.Load<Sprite>("Enemies/sample/Static/Front/Megapack III Behemoth");
-        enemy.defeatedSprite = Resources.Load<Sprite>("Enemies/sample/Static/Front/Megapack III Behemoth");
+        enemy.normalSprite = Resources.Load<Sprite>("Enemies/DubTapMusic/0001_takoyaki/0001_takoyaki_01");
+        enemy.damagedSprite = Resources.Load<Sprite>("Enemies/DubTapMusic/0001_takoyaki/0001_takoyaki_11");
+        enemy.defeatedSprite = Resources.Load<Sprite>("Enemies/DubTapMusic/0001_takoyaki/0001_takoyaki_21");
         enemyList.Add(enemy);
         
         // バトル中の敵を設定
@@ -56,29 +43,59 @@ public class BattleDataScript : MonoBehaviour
 
     void setEnemy()
     {
-        EnemyData enemy = enemyList[enemyIdx];
-        enemyNameText.text = enemy.name;
-        enemyHp = enemy.hp;
+        enemyData = enemyList[enemyIdx];
+        enemyNameText.text = enemyData.name;
+        enemyHp = enemyData.hp;
         hpText.text = enemyHp.ToString();
-        mainSpriteRenderer.sprite = enemy.normalSprite;
+        mainSpriteRenderer.sprite = enemyData.normalSprite;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (defeatedRemainingFrame > 0) {
+            damagedRemainingFrame = 0;
+            defeatedRemainingFrame--;
+            if (defeatedRemainingFrame == 0)
+            {
+                enemyIdx++;
+                if (enemyIdx > ENEMY_LIST_MAX_SIZE - 1)
+                {
+                    enemyIdx = 0;
+                }
+                setEnemy();
+                mainSpriteRenderer.sprite = enemyData.normalSprite;
+            }
+        }
+        else if (damagedRemainingFrame > 0)
+        {
+            damagedRemainingFrame--;
+            if (damagedRemainingFrame == 0)
+            {
+                mainSpriteRenderer.sprite = enemyData.normalSprite;
+            }
+        }
     }
 
     public void OnClickAttackButton() {
-        enemyHp--;
-        hpText.text = enemyHp.ToString();
-        if (enemyHp == 0) {
-            enemyIdx++;
-            if (enemyIdx > ENEMY_LIST_MAX_IDX) {
-                enemyIdx = 0;
+        if (defeatedRemainingFrame > 0) 
+        {
+            // do nothing
+        }
+        else {
+            enemyHp--;
+            hpText.text = enemyHp.ToString();
+            if (enemyHp == 0)
+            {
+                defeatedRemainingFrame = 90;
+                mainSpriteRenderer.sprite = enemyData.defeatedSprite;
             }
-            setEnemy();
-        } 
+            else
+            {
+                damagedRemainingFrame = 20;
+                mainSpriteRenderer.sprite = enemyData.damagedSprite;
+            }
+        }
     }
 }
 
--- README.md
+++ README.md
@@ -1,2 +1,73 @@
 # DubTapMania
  tap music game
+
+## TODO（済）
+・ボタンをクリックするとHPが減ってやっつけたら次の敵が出てくる
+・敵データのクラス化
+
+## TODO（なう）
+・敵スプライトの作成
+・敵スプライトのアニメーション
+
+## TODO（未）
+・背景の設定
+・戦闘曲のループ再生
+・タップ時の攻撃エフェクト
+・CircleCI、DeployGateを回してAndroidで簡単にテストできるようにする。
+・タイトルシーンとバトルシーンに分ける。
+・認証
+・マスタデータ取得APIとの通信
+　　敵データ（ドロップ情報含む）
+　　アイテムデータ（音と絡める）
+　　ストーリー情報（API化しないほうがよさそう）
+・課金システム
+・動画広告と音楽ゲームを絡める
+
+
+
+■Unity
+・Unityプロジェクトのバージョン管理にはGitHub Desktopを使おう
+https://qiita.com/hkomo746/items/a4f99261868069f6eeb5
+※Github for Unityはログインすらできなかった。
+
+・【Unity】スマホの縦向き横向きを設定する方法！画面の回転も制御できる
+https://clrmemory.com/programming/unity/apps-horizontal-vertical/
+
+・[Unity5.5] 2Dゲーム開発用に画面サイズを設定する
+https://design-program.com/unity/unity-2d_screen_size_setting
+
+・Unity入門！チュートリアルで学ぶ2Dアクションゲームの作り方
+https://udemy.benesse.co.jp/design/3d/unity-2d.html
+※スプライトの使い方
+https://udemy.benesse.co.jp/design/3d/unity-2d.html
+https://assetstore.unity.com/packages/2d/characters/aekashics-librarium-megapack-iii-130410
+
+・【Unity入門】Buttonの作り方!押された判定はどう取るの?
+https://www.sejuku.net/blog/56265
+
+・【C#】 Listから指定したIndexで取得(get)したい
+http://okaprogram.blog.fc2.com/blog-entry-7.html
+
+・【C#入門】文字列を数値に、数値を文字列に変換する方法
+https://www.sejuku.net/blog/44977
+
+・Unity オブジェクトの画像（Sprite)を動的に変更する方法
+https://qiita.com/motsat/items/927a4d2682765555b80d
+
+・C# の const の間違った使い方をやめよう
+https://qiita.com/Nossa/items/b874fa6c115898e2a9c8
+
+・C# Listの使い方のサンプル
+https://itsakura.com/csharp-list
+
+・Unityでデータ保持用のクラスを扱う時に戸惑ったお話
+https://collonville.hateblo.jp/entry/2017/09/27/021224
+
+・UnityのC#スクリプトでクラスやGameObjectなどを呼び出す手法集
+https://qiita.com/sabamotto/items/7aa3a988dd4cd45633db
+※namespaceとusing
+
+・Resources.Loadでスプライトを取得する
+http://matudozer.blog.fc2.com/blog-entry-28.html
+
+・スプライト（公式ドキュメント）
